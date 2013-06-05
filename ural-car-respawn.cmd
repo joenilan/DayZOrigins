324c7c1bf931c389a1652996f5bedc41b3f9904c
Joe Nilan <suburbanwhiteguy@gmail.com>
2013-06-05 16:28:20 -0400

    Removed my path

--- ural-car-respawn.cmd
+++ ural-car-respawn.cmd
@@ -1,9 +1,9 @@
-cd /D "C:\xampp\mysql\bin\"
+cd /D "C:\path\to\mysql\"
 mysql.exe --user=CHANGEME --password=CHANGEME --execute="call pCleanup()" dayz_origins
 mysql.exe --user=CHANGEME --password=CHANGEME --execute="call pMain()" dayz_origins
 mysql.exe --user=CHANGEME --password=CHANGEME --execute="call pMain()" dayz_origins
 mysql.exe --user=CHANGEME --password=CHANGEME --execute="call pMain()" dayz_origins
 mysql.exe --user=CHANGEME --password=CHANGEME --execute="call pMain()" dayz_origins
-mysql.exe --user=cCHANGEME --password=CHANGEME --execute="call pMain()" dayz_origins
+mysql.exe --user=CHANGEME --password=CHANGEME --execute="call pMain()" dayz_origins
 ping 127.0.0.1 -n 5 >NUL
 EXIT
